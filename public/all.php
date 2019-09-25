0359d4e65db11f2d9299736d832a3dbabc99a1c2
rexsteroxy <rexsteroxy22@gmail.com>
2019-09-25 16:53:35 +0100

    first code push

---
+++ public/all.php
@@ -0,0 +1,14 @@
+<div id="fh5co-register" style="background-image: url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 animate-box">
+				<div class="date-counter text-center">
+					<h2>Village Of Peace</h2>
+					<h3>By Tutu</h3>
+					<div class="simply-countdown simply-countdown-one"></div>
+					<p><strong>Holy Seminarians</strong></p>
+					<p><a href="#" class="btn btn-primary btn-lg btn-reg">Like Tutu</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ public/index.php
@@ -0,0 +1,133 @@
+<?php include('header.php');?>
+<?php include('slider.php');?>	
+
+	<div id="fh5co-course-categories">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Course categories</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-shop"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Church Doctrine</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-heart4"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Health &amp; Psychology</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-banknote"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Anthropology</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-lab2"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Phylosophy</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-photo"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Art &amp; Media</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-home-outline"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Greek</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-bubble3"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Language</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 text-center animate-box">
+					<div class="services">
+						<span class="icon">
+							<i class="icon-world"></i>
+						</span>
+						<div class="desc">
+							<h3><a href="#">Science And Technology</a></h3>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<!-- ends here -->
+	<!-- starts here -->
+	<div id="fh5co-course-categories">
+			<div class="container">
+				<div id="fh5co-gallery" class="fh5co-bg-section">
+						<div class="row text-center">
+							<h2><span>Seminary Gallery</span></h2>
+						</div>
+						<div class="row">
+							<div class="col-md-3 ">
+								<a href="#" class="gallery" style="background-image: url(images/img1.jpg);"></a>
+							</div>
+							<div class="col-md-3 ">
+								<a href="#" class="gallery" style="background-image: url(images/img2.jpg);"></a>
+							</div>
+							<div class="col-md-3 ">
+								<a href="#" class="gallery" style="background-image: url(images/img3.jpg);"></a>
+							</div>
+							<div class="col-md-3 ">
+								<a href="#" class="gallery" style="background-image: url(images/img4.jpg);"></a>
+							</div>
+						</div>
+                    </div>
+                    <button class="btn btn primay"> <a href="gallery.html">Click To View More</a> </button>
+					</div>
+					</div>
+<?php include('all.php');?>
+<?php include('footer.php');?>
+
+
