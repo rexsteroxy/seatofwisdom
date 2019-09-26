28b9affff44d43773e3b3d9ad7c8ee92c7e4a66b
rexsteroxy <rexsteroxy22@gmail.com>
2019-09-26 23:05:52 +0100

    worked on the fronted

--- public/all.php
+++ public/all.php
@@ -3,8 +3,8 @@
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2 animate-box">
 				<div class="date-counter text-center">
-					<h2>Village Of Peace</h2>
-					<h3>By Tutu</h3>
+					<h2>Seat Of Wisdom Seminary Ariam</h2>
+					<h3>Village Of Peace</h3>
 					<div class="simply-countdown simply-countdown-one"></div>
 					<p><strong>Holy Seminarians</strong></p>
 					<p><a href="#" class="btn btn-primary btn-lg btn-reg">Like Tutu</a></p>
