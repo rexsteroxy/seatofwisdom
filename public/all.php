7c4ec04af8fc27e602f5fa2359256dc304b13d2a
rexsteroxy <rexsteroxy22@gmail.com>
2019-09-27 08:43:23 +0100

    Edited the homepage

--- public/all.php
+++ public/all.php
@@ -3,11 +3,11 @@
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2 animate-box">
 				<div class="date-counter text-center">
-					<h2>Seat Of Wisdom Seminary Ariam</h2>
+					<h2>Welcome To Seat Of Wisdom Seminary Ariam</h2>
 					<h3>Village Of Peace</h3>
 					<div class="simply-countdown simply-countdown-one"></div>
-					<p><strong>Holy Seminarians</strong></p>
-					<p><a href="#" class="btn btn-primary btn-lg btn-reg">Like Tutu</a></p>
+					<p><h2>Rev.Fr.Dr.Virgilius Amadi</h2></p>
+					<p><a href="#" class="btn btn-primary btn-lg btn-reg">RECTOR</a></p>
 				</div>
 			</div>
 		</div>
