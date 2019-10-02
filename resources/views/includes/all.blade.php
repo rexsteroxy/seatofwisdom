f48871e7d9023612e67711f29abe691d668fa431
rexsteroxy <rexsteroxy22@gmail.com>
2019-10-02 15:41:15 +0100

    removed the timer in the home page

--- resources/views/includes/all.blade.php
+++ resources/views/includes/all.blade.php
@@ -5,7 +5,7 @@
 				<div class="date-counter text-center">
 					<h2>Welcome To Seat Of Wisdom Seminary Ariam</h2>
 					<h3>Village Of Peace</h3>
-					<div class="simply-countdown simply-countdown-one"></div>
+					<!-- <div class="simply-countdown simply-countdown-one"></div> -->
 					<p><h3>Rev.Fr.Dr.Virgilius Amadi</h3></p>
 					<p><a href="#" class="btn btn-primary btn-lg btn-reg">RECTOR</a></p>
 				</div>
